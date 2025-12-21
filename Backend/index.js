fb13e42e1dd5708e83fc9c7a5d7cfdbd5ed33cb6
samarth patel <samarthpatel981@gmail.com>
2025-12-21 21:47:51 +0530

    add path of frontend

--- Backend/index.js
+++ Backend/index.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://market-pulse-beta-seven.vercel.app",
     credentials: true,
   })
 );
